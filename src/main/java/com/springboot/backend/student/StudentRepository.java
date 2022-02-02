a38d47e42a22f6d3055a6e65393d9a2d4c2dbe62
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2022-02-02 11:30:29 -0500

    Get request mapping complete.  Student class => age given transient annotation and calculated in age getter via Period.between method

--- src/main/java/com/springboot/backend/student/StudentRepository.java
+++ src/main/java/com/springboot/backend/student/StudentRepository.java
@@ -1,5 +1,9 @@
 package com.springboot.backend.student;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
--- src/main/java/com/springboot/backend/student/StudentService.java
+++ src/main/java/com/springboot/backend/student/StudentService.java
@@ -1,24 +1,21 @@
 package com.springboot.backend.student;
 
-import com.sun.tools.javac.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
+import java.util.List;
 
 @Service
 public class StudentService {
 
-    public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Jackson",
-                        "jackson@dog.com",
-                        45,
-                        LocalDate.of(2000, Month.JANUARY, 5)
+    private final StudentRepository studentRepository;
 
-                )
-        );
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents(){
+        return studentRepository.findAll();
     }
 }
