d1d558569b20a0f008a56234ddc07a33127718a8
nicholasmcconnell <nicholas.mcconnell@gmail.com>
2022-02-03 09:46:29 -0500

    adjusted DB seed to use ArrayList insted of List from sun.tools as it was interering with creating and running multiple instances of the application

--- src/main/java/com/springboot/backend/student/StudentConfig.java
+++ src/main/java/com/springboot/backend/student/StudentConfig.java
@@ -1,12 +1,14 @@
 package com.springboot.backend.student;
 
-import com.sun.tools.javac.util.List;
+//import com.sun.tools.javac.util.List;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class StudentConfig {
@@ -14,8 +16,11 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository){
         return args -> {
+
+            List<Student> studentList = new ArrayList<>();
+
           Student jackson = new Student(
-                    "Jackson",
+                    "Jackon",
                     "jackson@dog.com",
                     LocalDate.of(2000, Month.JANUARY, 5)
             );
@@ -26,8 +31,11 @@ public class StudentConfig {
                     LocalDate.of(2006, Month.JANUARY, 5)
             );
 
+            studentList.add(jackson);
+            studentList.add(mariana);
+
             repository.saveAll(
-                    List.of(jackson, mariana)
+                    studentList
             );
         };
     }
